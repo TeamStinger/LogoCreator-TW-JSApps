708cb77bb7dedc91c7f1f38b3304be37c5e4e874
Ivorankov <ivo_rankov@mail.bg>
2015-08-28 12:36:43 +0300

    Additions to gallery

--- app/scripts/controllers/galleryController.js
+++ app/scripts/controllers/galleryController.js
@@ -1,11 +1,36 @@
 define([], function () {
     var GalleryController = {
         attachHandlers: function () {
-            $('.dropdown').on('click', function () {
-                $('.dropdown-menu').toggle();
+            $('#dropdownMenu1').on('click', function () {
+                $('#dropdown-menu-sort').toggle();
+            });
+
+            $('#grid-btn').on('click', function () {
+                var $btn = $(this),
+                    $icon = $btn.children().first(),
+                    $logos = $('.logos');
+
+                if ($icon.hasClass('glyphicon-th')) {
+                    $icon.removeClass('glyphicon-th');
+                    $icon.addClass('glyphicon-th-list');
+                    $logos.removeClass('col-md-4');
+                    $logos.addClass('col-md-12');
+
+                } else {
+                    $icon.removeClass('glyphicon-th-list');
+                    $icon.addClass('glyphicon-th');
+                    $logos.removeClass('col-md-12');
+                    $logos.addClass('col-md-4');
+                }
+
+            });
+
+            $('#dropdownMenu2').on('click', function () {
+                $('#dropdown-menu-category').toggle();
             });
         }
     };
 
     return GalleryController;
 });
+
