250c11a9f026241806e75c967884f262cd89ee6d
Ivorankov <ivo_rankov@mail.bg>
2015-08-28 17:16:18 +0300

    Smooth transition effect

--- app/scripts/controllers/galleryController.js
+++ app/scripts/controllers/galleryController.js
@@ -21,23 +21,29 @@ define([], function () {
                 $logoContainers = $('.thumbnail'),
                 $logosContent = $('.caption');
 
+            //Switch from grid view to list
             if ($btnIcon.hasClass('glyphicon-th')) {
                 $btnIcon.removeClass('glyphicon-th');
                 $btnIcon.addClass('glyphicon-th-list');
 
+                $logos.hide();
                 $logos.removeClass('col-md-4');
                 $logos.addClass('col-md-12');
+                $logos.fadeIn(800);
 
                 $logoContainers.removeClass('gallery-img-container-gridview');
 
                 $logosContent.removeClass('gallery-content-gridview');
 
+                //Switch from list view to grid
             } else {
                 $btnIcon.removeClass('glyphicon-th-list');
                 $btnIcon.addClass('glyphicon-th');
 
+                $logos.hide();
                 $logos.removeClass('col-md-12');
                 $logos.addClass('col-md-4');
+                $logos.fadeIn(800);
 
                 $logoContainers.addClass('gallery-img-container-gridview');
 
