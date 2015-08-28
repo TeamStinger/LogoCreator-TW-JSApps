7f33dda0867a9a420a194eeed35ad018c1f34081
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-08-28 18:28:12 +0300

    Add tab for chaging width and height of the logo

--- app/scripts/controllers/createLogoController.js
+++ app/scripts/controllers/createLogoController.js
@@ -3,31 +3,60 @@ define(['kendo'], function (kendo) {
 
     var CreateLogoController = {
         init: function () {
-            preview = $("#preview");
+            preview = $('#preview');
             CreateLogoController.createTabStrip();
             CreateLogoController.createColorPicker();
+            CreateLogoController.createSizeSliders();
         },
 
         createTabStrip: function () {
-            $("#tabstrip").kendoTabStrip({
+            $('#tabstrip').kendoTabStrip({
                 animation:  {
                     open: {
-                        effects: "fadeIn"
+                        effects: 'fadeIn'
                     }
                 }
             });
         },
 
         createColorPicker: function () {
-            $("#background").kendoFlatColorPicker({
+            $('#background').kendoFlatColorPicker({
                 preview: false,
-                value: "#000",
+                value: 'rgb(217, 89, 89)',
                 change: CreateLogoController.changeBackgroundColor
             });
         },
 
+        createSizeSliders: function () {
+            $('#width').kendoSlider({
+                min: 100,
+                max: 500,
+                value: 250,
+                smallStep: 10,
+                largeStep: 50,
+                slide: CreateLogoController.changeWidth
+            });
+
+            $('#height').kendoSlider({
+                min: 100,
+                max: 500,
+                value: 250,
+                smallStep: 10,
+                largeStep: 50,
+                slide: CreateLogoController.changeHeight
+            });
+        },
+
         changeBackgroundColor: function (event) {
-            preview.css("background-color", event.value);
+            preview.css('background-color', event.value);
+        },
+
+        changeWidth: function (event) {
+            preview.css('width', event.value);
+        },
+
+        changeHeight: function (event) {
+            preview.css('height', event.value);
         }
     };
 
