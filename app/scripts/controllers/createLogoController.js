2b48625984cb0c75667e75816a0a225f1e1d7ea8
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-08-28 19:08:39 +0300

    Add change font configurations

--- app/scripts/controllers/createLogoController.js
+++ app/scripts/controllers/createLogoController.js
@@ -1,12 +1,18 @@
 define(['kendo'], function (kendo) {
-    var preview;
+    var preview,
+        textPreview;
 
     var CreateLogoController = {
         init: function () {
             preview = $('#preview');
+            textPreview = $('#textPreview');
+
             CreateLogoController.createTabStrip();
             CreateLogoController.createColorPicker();
             CreateLogoController.createSizeSliders();
+            CreateLogoController.createFontSizeNumeric();
+            CreateLogoController.createLogoTextColorPicker();
+            CreateLogoController.attachAddTextHandler();
         },
 
         createTabStrip: function () {
@@ -47,6 +53,29 @@ define(['kendo'], function (kendo) {
             });
         },
 
+        createFontSizeNumeric: function () {
+            $('#changeFont').kendoNumericTextBox({
+                format: '#px',
+                min: 10,
+                max: 40,
+                step: 1,
+                value: 16,
+                spin: CreateLogoController.changeFontSize
+            });
+        },
+
+        createLogoTextColorPicker: function () {
+            $('#changeFontColor').kendoColorPicker({
+                value: '#ffffff',
+                buttons: false,
+                select: CreateLogoController.changeFontColor
+            });
+        },
+
+        attachAddTextHandler: function () {
+            $('#addText').on('click', CreateLogoController.addTextClick);
+        },
+
         changeBackgroundColor: function (event) {
             preview.css('background-color', event.value);
         },
@@ -57,6 +86,21 @@ define(['kendo'], function (kendo) {
 
         changeHeight: function (event) {
             preview.css('height', event.value);
+        },
+
+        changeFontSize: function () {
+            textPreview.css('font-size', this.value());
+        },
+
+        addTextClick: function (event) {
+            var text = $('#text');
+            textPreview.text(text.val());
+
+            event.preventDefault();
+        },
+
+        changeFontColor: function (event) {
+            textPreview.css('color', event.value);
         }
     };
 
