99f9e2f4c08f2816e72d01558fee7e3d1841c8d4
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-08-29 09:58:14 +0300

    Add AppendToDOM method in the renderer

--- app/scripts/utils/viewRenderer.js
+++ app/scripts/utils/viewRenderer.js
@@ -1,10 +1,22 @@
 define(['handlebars'], function (handlebars) {
     var ViewRenderer = {
         render: function (selector, view, data) {
+            var template = ViewRenderer.compileTemplate(selector, view, data);
+
+            $(selector).html(template);
+        },
+
+        appendToDOM: function (selector, view, data) {
+            var template = ViewRenderer.compileTemplate(selector, view, data);
+
+            $(selector).append(template);
+        },
+
+        compileTemplate: function (selector, view, data) {
             var template = handlebars.compile(view),
                 compiledTemplate = template(data);
 
-            $(selector).html(compiledTemplate);
+            return compiledTemplate;
         }
     };
 
