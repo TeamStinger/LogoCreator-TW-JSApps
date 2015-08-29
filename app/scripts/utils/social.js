815c3551ec94e7f104e8b7dfa758b817417e4f59
lsvilenov <svilenov.lubomir@gmail.com>
2015-08-29 11:20:32 +0300

    Added facebook like button in the gallery.

---
+++ app/scripts/utils/social.js
@@ -0,0 +1,9 @@
+/* --Facebook-- */
+(function(d, s, id) {
+    FB = null;
+    var js, fjs = d.getElementsByTagName(s)[0];
+    //if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=514076418757174";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
