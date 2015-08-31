1e2146f25c6636011bb15f869d03e1831a3d09d6
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-08-31 21:27:32 +0300

    Setup mocha test project

---
+++ tests/mainTest.js
@@ -0,0 +1,44 @@
+require.config({
+    baseUrl: '../app/scripts',
+    paths: {
+        'mocha': 'lib/mocha',
+        'chai': 'lib/chai',
+        'sinon': 'lib/sinon',
+        'jquery': 'lib/jquery',
+        'jqueryUI': 'lib/jquery-ui',
+        'text': 'lib/text',
+        'handlebars': 'lib/handlebars-v3.0.3',
+        'views': '../views',
+        'CryptoJS': 'lib/crypto-core-min',
+        'CryptoJS.SHA1': 'lib/crypto-sha1-min',
+        'lightBox': 'lib/lightbox.min',
+        'noty': 'lib/jquery.noty.packaged.min',
+        'kendo': 'lib/kendo.ui.core.min',
+        'html2canvas': 'lib/html2canvas',
+        'tests': '../../tests'
+    },
+    shim: {
+        'CryptoJS': {
+            exports: 'CryptoJS'
+        },
+        'CryptoJS.SHA1': {
+            deps: ['CryptoJS'],
+            exports: 'CryptoJS.SHA1'
+        },
+        'mocha': {
+            init: function () {
+                this.mocha.setup('bdd');
+
+                return this.mocha;
+            }
+        }
+    }
+});
+
+define(['mocha', 'jquery', 'jqueryUI', 'CryptoJS.SHA1', 'text', 'lightBox', 'noty'], function (mocha) {
+    require(['tests/utils/notifierTests'],
+        function () {
+            mocha.run();
+        });
+});
+
---
+++ tests/utils/notifierTests.js
@@ -0,0 +1,11 @@
+define(['chai', 'sinon', 'utils/notifier'], function (chai, sinon, notifier) {
+    var expect = chai.expect;
+
+    describe('Notifier', function () {
+        describe('showErrorMessage method', function () {
+            it('should call notify method', function () {
+                expect(true).to.be.true;
+            });
+        })
+    })
+});
