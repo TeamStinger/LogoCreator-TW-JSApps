6bca44e3813767ee2d5352cf3635a3b2b6e4f432
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-09-03 18:56:28 +0300

    Validate registration form

--- app/scripts/controllers/homeController.js
+++ app/scripts/controllers/homeController.js
@@ -1,5 +1,5 @@
-define(['../models/user', '../utils/notifier', '../utils/viewRenderer', 'text!views/about.html'],
-    function (user, notifier, viewRenderer, aboutTemplate) {
+define(['../models/user', '../utils/notifier', '../utils/viewRenderer', 'text!views/about.html', '../utils/validators'],
+    function (user, notifier, viewRenderer, aboutTemplate,validator) {
     var HomeController = {
         attachHandlers: function () {
             var registerForm = $('#registerForm'),
@@ -10,20 +10,35 @@ define(['../models/user', '../utils/notifier', '../utils/viewRenderer', 'text!vi
         },
 
         submitRegister: function (event) {
-            var formData = $(this).serializeArray();
-            var userData = {};
+            var formData = $(this).serializeArray(),
+                userData = {},
+                userName = formData[0].value,
+                password = formData[1].value,
+                email = formData[3].value;
 
-            formData.forEach(function (obj) {
-                userData[obj.name] = obj.value;
-            });
+            var isValidUserName = validator.userNameValidation(userName);
+            var isValidPassword = validator.passwordValidation(password);
+            var isValidEmail = validator.emailValidation(email);
 
-            user.register(userData)
-                .then(function (data) {
-                    $('#registerPanel').hide();
-                    notifier.showSuccessMessage('You are successfully registered!');
-                }, function (error) {
-                    notifier.showErrorMessage(error.message);
+            if (!isValidUserName.result) {
+                notifier.showErrorMessage(isValidUserName.error);
+            } else if (!isValidPassword.result) {
+                notifier.showErrorMessage(isValidPassword.error);
+            } else if (!isValidEmail.result) {
+                notifier.showErrorMessage(isValidEmail.error);
+            } else {
+                formData.forEach(function (obj) {
+                    userData[obj.name] = obj.value;
                 });
+
+                user.register(userData)
+                    .then(function (data) {
+                        $('#registerPanel').hide();
+                        notifier.showSuccessMessage('You are successfully registered!');
+                    }, function (error) {
+                        notifier.showErrorMessage(error.message);
+                    });
+            }
 
             event.preventDefault();
         },
