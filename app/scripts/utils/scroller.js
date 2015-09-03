ed365bd9fd104fd6c01f68ce7a06caac6d24a376
Ivorankov <ivo_rankov@mail.bg>
2015-09-03 18:37:23 +0300

    Scroll to top added

---
+++ app/scripts/utils/scroller.js
@@ -0,0 +1,20 @@
+define(['jquery'],function(){
+    $(document).ready(function(){
+
+        //Check to see if the window is top if not then display button
+        $(window).scroll(function(){
+            if ($(this).scrollTop() > 100) {
+                $('#scroll-btn').fadeIn();
+            } else {
+                $('#scroll-btn').fadeOut();
+            }
+        });
+
+        //Click event to scroll to top
+        $('.scrollToTop').click(function(){
+            $('html, body').animate({scrollTop : 0},800);
+            return false;
+        });
+
+    });
+});
